bc04032c00ce03f28d1e80a46b59b08a7dc48a96
Joe Dytrych <j@dytry.ch>
2016-06-21 00:27:16 +0100

    add cache headers

--- web-upload.sh
+++ web-upload.sh
@@ -10,3 +10,17 @@ s3cmd sync --rexclude '^(?!.*eakpackage).*$' ./ $1
 s3cmd sync --rexclude '^(?!.*bundles.*json).*$' ./ $1
 # Then, everything else
 s3cmd sync ./ $1
+
+# Headers: no-cache on HTML
+s3cmd modify \
+  --recursive \
+  --rexclude '^(?!.*\.html).*$' \
+  --add-header='Cache-Control: no-cache' \
+  $1
+
+# long cache timeout on eakpackage, kitten gifs, static assets
+s3cmd modify \
+  --recursive \
+  --rexclude '^(?!.*\.(static|(kitten-\d+\.gif)|eakpackage)).*$' \
+  --add-header='Cache-Control: public, max-age=31536000' \
+  $1
